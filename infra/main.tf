1236938a7e17c3defb4101cac70d40db25aabc74
EC2 Default User <ec2-user@ip-172-31-42-133.eu-west-1.compute.internal>
2024-11-18 18:05:44 +0000

    testing main branch terraform workflow again

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,5 @@
 #endring i main for å teste om terraform plan funker!!
+#endring
 #source: https://github.com/glennbechdevops/terraform-state/blob/main/lambda.tf
 resource "aws_iam_role" "lambda_exec_role" {
   assume_role_policy = jsonencode({
